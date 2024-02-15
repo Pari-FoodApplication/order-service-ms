e0126c3aae2195b251c7fe75bdd340048da81e71
pariram s <pari.pariram.ram@gmail.com>
2024-02-15 19:13:17 +0530

    allowed cross origin request

--- src/main/java/com/pari/foodapp/orderserviceapp/controller/OrderController.java
+++ src/main/java/com/pari/foodapp/orderserviceapp/controller/OrderController.java
@@ -6,11 +6,13 @@ import com.pari.foodapp.orderserviceapp.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class OrderController {
 
     @Autowired
